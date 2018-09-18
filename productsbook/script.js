c0c9058a082a67712159cb49fad3cfaac9594e8e
ramyapabbu <43370298+ramyapabbu@users.noreply.github.com>
2018-09-18 15:30:46 +0200

    update

--- productsbook/script.js
+++ productsbook/script.js
@@ -1,29 +1,36 @@
+console.log("start of js file");
+
 /* Fetch data from the json file into a javascript object */
 fetch("product.json")
 .then(function(response) {
     return response.json();
+    // console.log("inne i fetch");
 })
 .then(function(data) {
-    productdata = data;
-    createListFromproductData();
-
+    productData = data;
+    
+    console.log("fetch done");
     console.log(productData);
+
+    createListFromproductData();
 });
 
 //Skapa lista
 
 function createListFromproductData() {
     
+    // var productsBook = document.createElement("div");
+    // productBook.className = 'productsBookClass';
+
+    console.log("start of function");
     console.log(productData);
 
-        var productsBook = document.createElement("div");
-        productBook.className = 'productsBookClass';
 
     // Loopa genom listan
     for(var index = 0; index < productData.length; index++) {
-       
+        
+        console.log("inne i loopen");
     console.log(productData[index].productName);
-
     // FIRST WAY TO DO IT
 
     // var productCard = document.createElement("div");
@@ -35,37 +42,46 @@ function createListFromproductData() {
        // Create new card
 
         // SKAPA KORT VIA EN FUNKTION
-         var productCard = createproductCard(productData[index]);
+        var productCard = createproductCard(productData[index]);
 
       //Add card to book
-     productBook.appendChild(productCard);
+    productBook.appendChild(productCard);
        
     }
-
-    // SKRIV UT ADRESSBOKEN
-   // document.getElementById("adressBookContainer").appendChild(adressBook)
+    
+    console.log("fetch done");
+    // SKRIV UT PRODUCTBOKEN
+   // document.getElementById("productBookContainer").appendChild(productBook)
    // console.log(productBook);
-    document.body.appendChild(productBook);
+  //  document.body.appendChild(productBook);
 
  
  }
 
  function createproductCard(productData) {
    
+    console.log(productData);
+
     var productCard = document.createElement("div")
-    adressCard.className = 'adressCardClass';
+    productCard.className = 'productCardClass';
     
            // Name
             var printName = document.createElement("h3");
             printName.innerText = "Name: "  + productData.productName;
             //console.log(printName);
             productCard.appendChild(printName);
-    
+
+           //Price
+            var printPrice = document.createElement("h3");
+            printPrice.innerText = "Price: "  + productData.productPrice;
+            //console.log(printPrice);
+            productCard.appendChild(printPrice);
+            
             //Image
             var printImage = document.createElement("img");
             printImage.src = "bild/" + productData.productImage;
             productCard.appendChild(printImage);
     
-           return productCard;
+          // return productCard;
 }
 
